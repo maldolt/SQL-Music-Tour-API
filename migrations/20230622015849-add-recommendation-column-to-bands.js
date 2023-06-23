32efc3160096f6390bd78425fb5dc94f097a60be
Laura Velasco <maldolt@nv.ccsd.net>
2023-06-23 15:08:51 -0700

    first commit

---
+++ migrations/20230622015849-add-recommendation-column-to-bands.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn(
+      'band',
+      'recommendation',
+      {type: DataTypes.STRING}
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
